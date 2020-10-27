a72117ab4028c843f78fc4165934fd4d74cab015
pavan.reddy <saipavanreddyr21@gmail.com>
2020-10-27 15:37:07 +0530

    added src

--- react-app/App.jsx
+++ react-app/App.jsx
@@ -1,10 +1,11 @@
 
 import React from "react"
+import Demo from './src/Demo'
 class App extends React.Component{
     render(){
         return(
             <div> WElcome to react app 
-               
+               <Demo/>
             </div>
         );
     }
