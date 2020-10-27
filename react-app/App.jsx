c98cc841becf7d542584db40f53b5a10d4398857
pavan.reddy <saipavanreddyr21@gmail.com>
2020-10-27 12:52:34 +0530

    react- webpack configuration -running proj

--- react-app/App.jsx
+++ react-app/App.jsx
@@ -1,12 +1,13 @@
-import 'React' from 'react'
 
+import React from "react"
 class App extends React.Component{
     render(){
-        return (<div>
-            WELCOME !!!!
-        </div>)
+        return(
+            <div> WElcome to react app 
+               
+            </div>
+        );
     }
 }
-
 
 export default App;
